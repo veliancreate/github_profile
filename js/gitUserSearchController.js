40d5613f6f27616dba7a2c7f9d087ae947c21f49
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-03-19 13:36:56 +0000

    http request logic works. need to refactor to include github key

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,19 +1,19 @@
 githubUserSearch.controller('GitUserSearchController', function($scope, $resource, $http) {
   var searchResource = $resource('https://api.github.com/search/users');
-
   $scope.doSearch = function (){
-
     $scope.searchResult = searchResource.get({
-      access_params: "d1ba947cc408fd93882dd477daffe48284fe1a31", 
+      access_params: "11dbfa66739fa0c7c70fb19983eaa1408de60b46", 
       q: $scope.searchTerm,
     }, function(){
-      var name = 'https://api.github.com/users/' + $scope.searchTerm;
-      $http.get(name).
-        success(function(data){
-          $scope.individualData = data     
-        });
+      angular.forEach($scope.searchResult.items, function(v){
+        var name = 'https://api.github.com/users/' + v.login;
+        $http.get(name).success(function(data){
+          $scope.individualData = data;
+          v.followers = $scope.individualData.followers;
+          v.public_repos = $scope.individualData.public_repos;     
+        });        
+      });
     });
-
   };
 
 });
